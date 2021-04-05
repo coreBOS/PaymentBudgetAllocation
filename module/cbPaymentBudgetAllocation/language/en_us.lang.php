aec98f4dde8471af5879efaa5c0c21ccf989bb91
glorymoshi <glorysimon95@gmail.com>
2021-04-05 18:35:23 +0300

    feat(module)create module cbPaymentBudgetAllocation

---
+++ module/cbPaymentBudgetAllocation/language/en_us.lang.php
@@ -0,0 +1,28 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'cbPaymentBudgetAllocation' => 'Payment-Budget Allocations',
+	'SINGLE_cbPaymentBudgetAllocation' => 'Payment-Budget Allocation',
+	'cbPaymentBudgetAllocation ID' => 'Payment-BA ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_cbPaymentBudgetAllocation_INFORMATION' => 'Payment-Budget Allocation Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+
+	'PaymentBudgetAllocation' => 'Payment-Budget Allocation',
+	'Payment Budget Allocation No' => 'Payment-BA No',
+	'Payment' => 'Payment',
+	'Budget Item' => 'Budget Item',
+	'Percentage' => 'Percentage',
+	'Allocated Amount' => 'Allocated Amount',
+
+);
+?>
